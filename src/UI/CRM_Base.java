a7d97358c80e9d2f98880bd85caeb5f3e3104711
rafalp86 <rafalpilecki@wp.pl>
2014-11-10 20:11:15 +0100

    build fix

--- src/UI/CRM_Base.java
+++ src/UI/CRM_Base.java
@@ -66,9 +66,7 @@ public class CRM_Base  implements SauceOnDemandSessionIdProvider, SauceOnDemandA
 	
 	protected  void Screenshot(String fileName) {
         try {
-        	File ScreenshotDir = new File("Screenshot");
-        	if (!ScreenshotDir.exists()) ScreenshotDir.mkdir();
-            FileOutputStream out = new FileOutputStream(".\\Screenshot\\" + fileName + "_CRM.png");
+            FileOutputStream out = new FileOutputStream(fileName + "_CRM.png");
             out.write(((org.openqa.selenium.TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
             out.close();
         } catch (Exception e) {
