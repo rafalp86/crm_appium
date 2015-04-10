2f727b3701fd22563877cd74348b37dfcaf018a8
rafalp86 <rafalpilecki@wp.pl>
2015-04-10 19:31:03 +0200

    .

--- BuildScipts/runAppiumServer.ps1
+++ BuildScipts/runAppiumServer.ps1
@@ -19,6 +19,7 @@ Write-Host $AppiumProcessCount , $SeleniumProcessCount
 if($AppiumProcessCount -eq 0)
 {
     "Run Appium server"
+    "Appium dir:"+$Env:APPIUM
     Start-Process -FilePath ($Env:APPIUM+"\..\..\node.exe") -ArgumentList  ($Env:APPIUM+"\lib\server\main.js --address 127.0.0.1 --port 4723 --app "+$apkFile+" --avd "+$EmulatorName+" --log-timestamp --log c:\AppiumForm.txt --platform-name Android --platform-version 19 --automation-name Appium --log-no-color")
 }
 #else
@@ -31,7 +32,7 @@ if($SeleniumProcessCount -eq 0)
 {
 "Run selenium server"
 $serwerPATH=$Env:SELENIUM+"\selenium-server-standalone-2.45.0.jar"
-$serwerPATH
+"Selenium Dir :"+$serwerPATH
    #  Start-Process -FilePath java  -ArgumentList ("-jar "+$serwerPATH+"")
 }
 
